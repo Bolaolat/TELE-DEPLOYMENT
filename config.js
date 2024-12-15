bb41f3409ada9ec8ad86565070c9e5f6707ea636
Bolaolat <bolaolat300@gmail.com>
2024-12-15 18:05:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 module.exports = {
-    TELEGRAM_API_TOKEN: '7849087949:AAHEuYKeENyVhDMGRoh2bbkp_zJOdqnJ2zA'
+    TELEGRAM_API_TOKEN: '7999085008:AAFd2E5igQgUbmN1WHVO0TSIZdlCY-konUs'
 };
